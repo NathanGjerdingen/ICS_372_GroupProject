ee5769c9cb3121a77b7781dca2a4b743197f1ae0
Nanofu <Nanofu@users.noreply.github.com>
2018-10-10 19:33:45 -0500

    Add constructor and discription
    Changed getPhoneNumber to custPhoneNumber. Added description to all methods.

--- src/Customer.java
+++ src/Customer.java
@@ -1,34 +1,73 @@
 
 public class Customer {
 
-	private int getPhoneNumber;
+	private int custPhoneNumber;
 	private String name;
 	private int customerID;
 
+	/**
+	 * Creates a customer object with the customer information.
+	 * 
+	 * @param name
+	 * @param phoneNumber
+	 * @param customerID
+	 */
 	public Customer(String name, int phoneNumber, int customerID) {
-		// TODO Auto-generated constructor stub
+		this.name = name;
+		custPhoneNumber = phoneNumber;
+		this.customerID = customerID;
 	}
 
+	/**
+	 * Return customer phone number
+	 * 
+	 * @return customer's phone number
+	 */
 	public int getGetPhoneNumber() {
-		return getPhoneNumber;
+		return custPhoneNumber;
 	}
 
+	/**
+	 * Set phone number
+	 * 
+	 * @param getPhoneNumber
+	 */
 	public void setGetPhoneNumber(int getPhoneNumber) {
-		this.getPhoneNumber = getPhoneNumber;
+		this.custPhoneNumber = getPhoneNumber;
 	}
 
+	/**
+	 * Return customer name
+	 * 
+	 * @return customer name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Set customer name
+	 * 
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Return customer's ID
+	 * 
+	 * @return customer's ID
+	 */
 	public int getCustomerID() {
 		return customerID;
 	}
 
+	/**
+	 * Set customer's ID
+	 * 
+	 * @param customerID
+	 */
 	public void setCustomerID(int customerID) {
 		this.customerID = customerID;
 	}
